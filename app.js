f1f825d9a65198c3fa4ed39ce94936d3360425ae
devmalya99 <144609723+devmalya99@users.noreply.github.com>
2023-09-17 21:38:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=[]{}\\|;':\",./<>?";
+
+const lengthEl = document.getElementById("length");
+const lowercaseEl = document.getElementById("lowercase");
+const uppercaseEl = document.getElementById("uppercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const passwordEl = document.getElementById("password");
+const generateBtn = document.getElementById("generate")
+
+generateBtn.addEventListener("click",function(){
+
+const length=lengthEl.value;
+let password= "";
+let characters="";
+
+if(lowercaseEl.checked)
+{
+    characters+=lowercaseLetters;
+}
+
+if(uppercaseEl.checked)
+{
+    characters+=uppercaseLetters;
+}
+
+if(numbersEl.checked)
+{
+    characters+=numbers;
+}
+
+if(symbolsEl.checked)
+{
+    characters+=symbols;
+}
+
+for(let i=0;i<length;i++)
+{
+    password +=characters.charAt(Math.floor(Math.random()*characters.length))
+}
+
+passwordEl.value=password;
+
+});
